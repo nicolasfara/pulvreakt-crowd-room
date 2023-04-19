e789100b4351450a3545a9baa5724c1ac1b2ea44
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-04-19 16:25:55 +0200

    chore: set log level to info

--- src/jvmMain/kotlin/it/nicolasfarabegoli/pulverization/crowd/room/RoomMain.kt
+++ src/jvmMain/kotlin/it/nicolasfarabegoli/pulverization/crowd/room/RoomMain.kt
@@ -1,7 +1,11 @@
 package it.nicolasfarabegoli.pulverization.crowd.room
 
+import co.touchlab.kermit.Logger
+import co.touchlab.kermit.Severity
+
 suspend fun main(args: Array<String>) {
     require(args.size == 2) { "usage: java -jar smartphone.jar device-id host" }
+    Logger.setMinSeverity(Severity.Info)
     val deviceId = args[0]
     val host = args[1]
     roomProgram(deviceId, host)
