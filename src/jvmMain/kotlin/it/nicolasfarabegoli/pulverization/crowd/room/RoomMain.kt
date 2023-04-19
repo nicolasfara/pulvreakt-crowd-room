080f3d320ac70d3ab5a27a5ddc0d2b4811a58de9
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-04-19 17:51:39 +0200

    chore: enable debug log via environment variable

--- src/jvmMain/kotlin/it/nicolasfarabegoli/pulverization/crowd/room/RoomMain.kt
+++ src/jvmMain/kotlin/it/nicolasfarabegoli/pulverization/crowd/room/RoomMain.kt
@@ -5,7 +5,9 @@ import co.touchlab.kermit.Severity
 
 suspend fun main(args: Array<String>) {
     require(args.size == 2) { "usage: java -jar smartphone.jar device-id host" }
-    Logger.setMinSeverity(Severity.Info)
+    val debugVar = System.getenv("DEBUG_LOG_LEVEL")
+    val severity = if (debugVar == null || debugVar == "0") Severity.Info else Severity.Debug
+    Logger.setMinSeverity(severity)
     val deviceId = args[0]
     val host = args[1]
     roomProgram(deviceId, host)
